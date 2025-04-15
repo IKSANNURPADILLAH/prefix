73fd51f8495150b33d8b71434b5aca7e22f970c2
iksan nurpadillah <211210016@unmuhpnk.ac.id>
2025-04-15 10:17:56 +0700

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # === CONFIGURASI ===
-INTERFACE="eth0"                     # Ganti sesuai interface kamu (cek dengan `ip a`)
+INTERFACE="eth0"                      # Ganti sesuai interface kamu (cek dengan `ip a`)
 IP_PREFIX="5.231.234"                 # Prefix dari subnet kamu
-START=0                             # Awal range IP
+START=0                               # Awal range IP
 END=255                               # Akhir range IP
-EXCLUDE=(23 89)                   # IP akhir yang ingin dikecualikan, misal: 5.230.48.72, .80, .88
-PORT_START=3128                      # Port pertama untuk Squid (akan naik terus)
-USERNAME="vodkaace"
-PASSWORD="indonesia"
+EXCLUDE=(23 89)                       # IP akhir yang ingin dikecualikan, misal: 5.230.48.72, .80, .88
+PORT_START=3128                       # Port pertama untuk Squid (akan naik terus)
+USERNAME="vodkaace"                   # Username Squid
+PASSWORD="indonesia"                  # Password squid
 PASSWD_FILE="/etc/squid/passwd"
 SQUID_CONF="/etc/squid/squid.conf"
-HASIL_FILE="hasil.txt"
-NETMASKS="24"
+HASIL_FILE="hasil.txt"                # Letak penyimpanan hasil file 
+NETMASKS="24"                         # Sesuaikan subnet
 # === FUNGSI UNTUK CEK APAKAH ANGKA ADA DI EXCLUDE ===
 is_excluded() {
     local num=$1
